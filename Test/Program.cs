6acd1b5861158266416f9d8731a3fc4f3e6d4329
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 13:40:36 -0400

    added test project

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Test;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
