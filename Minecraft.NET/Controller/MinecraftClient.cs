d54793e16cfffc6c17b8ea008ed73c06456e413c
Ryan <ryanthornton03@gmail.com>
2023-07-24 00:54:57 +0100

    Added DownloadAllLibs()

--- Minecraft.NET/Controller/MinecraftClient.cs
+++ Minecraft.NET/Controller/MinecraftClient.cs
@@ -18,36 +18,39 @@ public class MinecraftClient : IDisposable
 
     public async Task DownloadAllLibs()
     {
+        string libBasePath = Path.Combine(_clientStartInfo.Directory, "libraries");
         string url = "https://piston-meta.mojang.com/v1/packages/715ccf3330885e75b205124f09f8712542cbe7e0/1.20.1.json";
-        using (HttpClient client = new HttpClient())
-        {
-            try
-            {
-                HttpResponseMessage response = await client.GetAsync(url);
-                if (response.IsSuccessStatusCode)
-                {
-                    string json = await response.Content.ReadAsStringAsync();
-                    dynamic data = JsonConvert.DeserializeObject(json);
-                    foreach (var library in data.libraries)
-                    {
-                        string libUrl = library.url;
-                        string libPath = library.path;
 
-                        Console.WriteLine("URL: " + libUrl);
-                        Console.WriteLine("Path: " + libPath);
-                        Console.WriteLine();
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Request failed with status code: " + response.StatusCode);
-                }
-            }
-            catch (Exception ex)
+        HttpResponseMessage response = await _client.GetAsync(url);
+
+        if (response.IsSuccessStatusCode)
+        {
+            string json = await response.Content.ReadAsStringAsync();
+            dynamic data = JsonConvert.DeserializeObject(json);
+            foreach (var library in data.libraries)
             {
-                Console.WriteLine("An error occurred: " + ex.Message);
+                string libUrl = library.downloads.artifact.url;
+                string libPath = library.downloads.artifact.path;
+
+                Console.WriteLine("URL: " + libUrl);
+                Console.WriteLine("Path: " + libPath);
+
+
+                //await _client.DownloadFileAsync(new Uri(libUrl), Directory.CreateDirectory(Path.Combine(libBasePath, Path.GetDirectoryName(libPath))).FullName, (s, e)=>{ });
+
+                string directoryPath = Path.GetDirectoryName(libPath);
+                Directory.CreateDirectory(directoryPath);
+
+                await _client.DownloadFileAsync(new Uri(libUrl), libPath, (s, e) => { });
+
+                Console.WriteLine();
             }
         }
+        else
+        {
+            Console.WriteLine("Request failed with status code: " + response.StatusCode);
+        }
+
     }
 
     public void Dispose()
--- Test/MinecraftTest.cs
+++ Test/MinecraftTest.cs
@@ -1,4 +1,6 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
+using Chase.Minecraft.Controller;
+
 namespace Test;
 
 internal static class MinecraftTest
@@ -6,5 +8,8 @@ internal static class MinecraftTest
     public static async Task Start()
     {
         //        Process.Start("explorer.exe");
+        MinecraftClient minecraftClient = new MinecraftClient(new(){Directory = "", JVMArguments = "", RAM = new() {} });
+        await minecraftClient.DownloadAllLibs();
+        Console.ReadLine();
     }
 }
