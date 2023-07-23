f462f4095070a643f5f39651d27abe0b359526c8
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 15:31:32 -0400

    added project version files

---
+++ Modrinth.NET/Model/ModrinthVersionFile.cs
@@ -0,0 +1,102 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Modrinth.Model;
+
+public struct ModrinthVersionFile
+{
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("version_number")]
+    public string VersionNumber { get; set; }
+
+    [JsonProperty("changelog")]
+    public string Changelog { get; set; }
+
+    [JsonProperty("dependencies")]
+    public Dependency[] Dependencies { get; set; }
+
+    [JsonProperty("game_versions")]
+    public string[] GameVersions { get; set; }
+
+    [JsonProperty("version_type")]
+    public string VersionType { get; set; }
+
+    [JsonProperty("loaders")]
+    public string[] Loaders { get; set; }
+
+    [JsonProperty("featured")]
+    public bool Featured { get; set; }
+
+    [JsonProperty("status")]
+    public string Status { get; set; }
+
+    [JsonProperty("requested_status")]
+    public string RequestedStatus { get; set; }
+
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("project_id")]
+    public string ProjectId { get; set; }
+
+    [JsonProperty("author_id")]
+    public string AuthorId { get; set; }
+
+    [JsonProperty("date_published")]
+    public string DatePublished { get; set; }
+
+    [JsonProperty("downloads")]
+    public int Downloads { get; set; }
+
+    [JsonProperty("changelog_url")]
+    public string ChangelogUrl { get; set; }
+
+    [JsonProperty("files")]
+    public VersionFileDetails[] Files { get; set; }
+}
+
+public struct Dependency
+{
+    [JsonProperty("version_id")]
+    public string VersionId { get; set; }
+
+    [JsonProperty("project_id")]
+    public string ProjectId { get; set; }
+
+    [JsonProperty("file_name")]
+    public string FileName { get; set; }
+
+    [JsonProperty("dependency_type")]
+    public string DependencyType { get; set; }
+}
+
+public struct VersionFileDetails
+{
+    public Hashes hashes { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("filename")]
+    public string Filename { get; set; }
+
+    [JsonProperty("primary")]
+    public bool Primary { get; set; }
+
+    [JsonProperty("size")]
+    public int Size { get; set; }
+
+    [JsonProperty("file_type")]
+    public string FileType { get; set; }
+}
+
+public struct Hashes
+{
+    [JsonProperty("sha512")]
+    public string Sha512 { get; set; }
+
+    [JsonProperty("sha1")]
+    public string Sha1 { get; set; }
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,10 +1,30 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
+using Chase.Minecraft.Modrinth;
+using Chase.Minecraft.Modrinth.Model;
+using Newtonsoft.Json;
+
 namespace Test;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        using ModrinthClient client = new();
+        FacetBuilder builder = new FacetBuilder()
+            .AddVersions("1.19.4")
+            .AddProjectTypes(ModrinthProjectTypes.Mod)
+            .AddModloaders(Chase.Minecraft.ModLoaders.Fabric);
+        ModrinthSearchQuery query = new()
+        {
+            Facets = builder,
+            Query = "The Warp Mod",
+            Limit = 2,
+            Ordering = SearchOrdering.Relevance,
+        };
+
+        ModrinthSearchResult? results = client.Search(query);
+        Console.WriteLine(JsonConvert.SerializeObject(results, Formatting.Indented));
+        Console.Write("Press any key to continue...");
+        Console.ReadKey();
     }
 }
