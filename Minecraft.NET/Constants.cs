cbbf02d396adff861ba6cfcfab3e35afe4753887
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 19:11:40 -0400

    added constants

---
+++ Minecraft.NET/Constants.cs
@@ -0,0 +1,7 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft;
+
+public static class Constants
+{
+    public static readonly string GAME_VERSION_MANIFEST = "https://launchermeta.mojang.com/mc/game/version_manifest.json";
+}
