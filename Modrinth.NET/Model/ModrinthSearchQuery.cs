578096583daf4729e75e2bba13fc1606c015193a
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 15:02:59 -0400

    added default values

--- Modrinth.NET/Model/ModrinthSearchQuery.cs
+++ Modrinth.NET/Model/ModrinthSearchQuery.cs
@@ -3,9 +3,17 @@ namespace Chase.Minecraft.Modrinth.Model;
 
 public struct ModrinthSearchQuery
 {
-    public string Query { get; set; }
-    public SearchOrdering Ordering { get; set; }
-    public int Limit { get; set; }
-    public int Offset { get; set; }
-    public ModLoaders[] Modloaders { get; set; }
+    public string Query { get; set; } = "";
+
+    public SearchOrdering Ordering { get; set; } = SearchOrdering.Relevance;
+
+    public int Limit { get; set; } = 10;
+
+    public int Offset { get; set; } = 0;
+
+    public FacetBuilder? Facets { get; set; } = null;
+
+    public ModrinthSearchQuery()
+    {
+    }
 }
