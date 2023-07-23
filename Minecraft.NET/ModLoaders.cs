ad2e7b2a5d2f524431660d93ff1c9351c94ae23c
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 14:06:17 -0400

    added mod loaders enum

---
+++ Minecraft.NET/ModLoaders.cs
@@ -0,0 +1,13 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft;
+
+public enum ModLoaders
+{
+    None,
+    Forge,
+    Fabric,
+    Rift,
+    LiteLoader,
+    ModLoaderMP,
+    MCP
+}
