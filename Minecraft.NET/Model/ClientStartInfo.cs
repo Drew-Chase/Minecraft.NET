91e47db0a08241e200bd3c1875449f1fabf4e30f
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 19:11:29 -0400

    added client start info

---
+++ Minecraft.NET/Model/ClientStartInfo.cs
@@ -0,0 +1,24 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft.Model;
+
+public struct ClientStartInfo
+{
+    public required string Directory { get; set; }
+
+    public required string JVMArguments { get; set; }
+
+    public string? OfflineUsername { get; set; } = null;
+
+    public required RAMInfo RAM { get; set; }
+
+    public ClientStartInfo()
+    {
+    }
+}
+
+public struct RAMInfo
+{
+    public int MaximumRamMB { get; set; }
+    public int MinimumRamMB { get; set; }
+    public int PermGenSizeMB { get; set; }
+}
