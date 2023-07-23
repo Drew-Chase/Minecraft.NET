cee69ebae6f14ef6d0cdc4fbbff3835e5a047008
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 15:31:47 -0400

    added dependencies

---
+++ Modrinth.NET/Model/ModrinthProjectDependencies.cs
@@ -0,0 +1,8 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft.Modrinth.Model;
+
+public struct ModrinthProjectDependencies
+{
+    public ModrinthProject[] Projects { get; set; }
+    public ModrinthVersionFile[] Versions { get; set; }
+}
