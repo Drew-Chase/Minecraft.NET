955f08171a2f9cc8bc2cfb9630d8f25fb805fea7
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 14:06:28 -0400

    added search query

---
+++ Modrinth.NET/Model/ModrinthSearchQuery.cs
@@ -0,0 +1,11 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft.Modrinth.Model;
+
+public struct ModrinthSearchQuery
+{
+    public string Query { get; set; }
+    public SearchOrdering Ordering { get; set; }
+    public int Limit { get; set; }
+    public int Offset { get; set; }
+    public ModLoaders[] Modloaders { get; set; }
+}
