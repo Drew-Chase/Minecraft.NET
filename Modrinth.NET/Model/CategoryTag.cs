dc890a14dcfb299de53415b0a63f7aef8aa5851c
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 19:11:48 -0400

    added category tags

---
+++ Modrinth.NET/Model/CategoryTag.cs
@@ -0,0 +1,19 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Modrinth.Model;
+
+public struct CategoryTag
+{
+    [JsonProperty("icon")]
+    public string Icon { get; set; }
+
+    [JsonProperty("name")]
+    public string Name { get; set; }
+
+    [JsonProperty("project_type")]
+    public string ProjectType { get; set; }
+
+    [JsonProperty("header")]
+    public string Header { get; set; }
+}
