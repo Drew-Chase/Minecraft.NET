39a9ef30b6cef024d33406360b04269976c36a6b
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 15:03:18 -0400

    added json mappings

--- Modrinth.NET/Model/ModrinthSearchResultItem.cs
+++ Modrinth.NET/Model/ModrinthSearchResultItem.cs
@@ -1,27 +1,70 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
 namespace Chase.Minecraft.Modrinth.Model;
 
 public struct ModrinthSearchResultItem
 {
+    [JsonProperty("slug")]
     public string Slug { get; set; }
+
+    [JsonProperty("title")]
     public string Title { get; set; }
+
+    [JsonProperty("description")]
     public string Description { get; set; }
+
+    [JsonProperty("categories")]
     public string[] Categories { get; set; }
+
+    [JsonProperty("client_side")]
     public SideRequirements ClientSide { get; set; }
+
+    [JsonProperty("server_side")]
     public SideRequirements ServerSide { get; set; }
+
+    [JsonProperty("project_type")]
     public string ProjectType { get; set; }
+
+    [JsonProperty("downloads")]
     public int Downloads { get; set; }
+
+    [JsonProperty("icon_url")]
     public string IconUrl { get; set; }
+
+    [JsonProperty("color")]
     public int Color { get; set; }
+
+    [JsonProperty("project_id")]
     public string ProjectId { get; set; }
+
+    [JsonProperty("author")]
     public string Author { get; set; }
+
+    [JsonProperty("display_categories")]
     public string[] DisplayCategories { get; set; }
+
+    [JsonProperty("versions")]
     public string[] Versions { get; set; }
+
+    [JsonProperty("follows")]
     public int Follows { get; set; }
+
+    [JsonProperty("date_created")]
     public DateTime DateCreated { get; set; }
+
+    [JsonProperty("date_modified")]
     public DateTime DateModified { get; set; }
+
+    [JsonProperty("latest_version")]
     public string LatestVersion { get; set; }
+
+    [JsonProperty("license")]
     public string License { get; set; }
+
+    [JsonProperty("gallery")]
     public string[] Gallery { get; set; }
+
+    [JsonProperty("featured_gallery")]
     public string FeaturedGallery { get; set; }
 }
