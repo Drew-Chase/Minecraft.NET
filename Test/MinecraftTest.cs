21acf9ce4111b3ba3c8afc62dc885f22153c4a77
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 19:10:14 -0400

    added minecraft test

---
+++ Test/MinecraftTest.cs
@@ -0,0 +1,10 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Test;
+
+internal static class MinecraftTest
+{
+    public static async Task Start()
+    {
+        //        Process.Start("explorer.exe");
+    }
+}
