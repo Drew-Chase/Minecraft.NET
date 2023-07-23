18fa2d3dafa6e6a01fbfe20b5874e304d7d1f712
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 16:09:58 -0400

    added download version file

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,7 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
 using Chase.Minecraft.Modrinth;
 using Chase.Minecraft.Modrinth.Model;
+using System.Diagnostics;
 
 namespace Test;
 
@@ -8,18 +9,19 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
-        await SearchModrinth();
-        await GetModrinthProject();
-        await GetModrinthProjectDependencies();
-        await GetModrinthUser();
-        await GetProjectVersions();
+        using ModrinthClient client = new();
+        await SearchModrinth(client);
+        await GetModrinthProject(client);
+        await GetModrinthProjectDependencies(client);
+        await GetModrinthUser(client);
+        await GetProjectVersions(client);
+        await DownloadVersionFile(client);
         Console.Write("Press any key to continue...");
         Console.ReadKey();
     }
 
-    private static async Task SearchModrinth()
+    private static async Task SearchModrinth(ModrinthClient client)
     {
-        using ModrinthClient client = new();
         FacetBuilder builder = new FacetBuilder()
             .AddVersions("1.19.4")
             .AddProjectTypes(ModrinthProjectTypes.Mod)
@@ -39,9 +41,8 @@ internal class Program
         Console.WriteLine($" Modrinth Search!");
     }
 
-    private static async Task GetModrinthProject()
+    private static async Task GetModrinthProject(ModrinthClient client)
     {
-        using ModrinthClient client = new();
         ModrinthProject? project = await client.GetProjectAsync("ewLFY6nv");
         Console.ForegroundColor = project == null ? ConsoleColor.Red : ConsoleColor.Green;
         Console.Write($"[{(project == null ? "FAIL" : "SUCCESS")}]");
@@ -49,9 +50,8 @@ internal class Program
         Console.WriteLine($" Get Modrinth Project!");
     }
 
-    private static async Task GetModrinthProjectDependencies()
+    private static async Task GetModrinthProjectDependencies(ModrinthClient client)
     {
-        using ModrinthClient client = new();
         ModrinthProjectDependencies? dependencies = await client.GetProjectDependenciesAsync("ewLFY6nv");
         Console.ForegroundColor = dependencies == null ? ConsoleColor.Red : ConsoleColor.Green;
         Console.Write($"[{(dependencies == null ? "FAIL" : "SUCCESS")}]");
@@ -59,9 +59,8 @@ internal class Program
         Console.WriteLine($" Get Modrinth Project Dependencies!");
     }
 
-    private static async Task GetModrinthUser()
+    private static async Task GetModrinthUser(ModrinthClient client)
     {
-        using ModrinthClient client = new();
         ModrinthUser? user = await client.GetUserAsync("dcmanproductions");
         Console.ForegroundColor = user == null ? ConsoleColor.Red : ConsoleColor.Green;
         Console.Write($"[{(user == null ? "FAIL" : "SUCCESS")}]");
@@ -69,12 +68,34 @@ internal class Program
         Console.WriteLine($" Get Modrinth User!");
     }
 
-    private static async Task GetProjectVersions()
+    private static async Task GetProjectVersions(ModrinthClient client)
     {
-        using ModrinthClient client = new();
         var versions = await client.GetProjectVersionsAsync("ewLFY6nv");
         Console.ForegroundColor = versions == null ? ConsoleColor.Red : ConsoleColor.Green;
         Console.Write($"[{(versions == null ? "FAIL" : "SUCCESS")}]");
+        Console.ResetColor();
+        Console.WriteLine($" Get Project Versions!");
+    }
+
+    private static async Task DownloadVersionFile(ModrinthClient client)
+    {
+        var versions = await client.GetProjectVersionsAsync("ewLFY6nv");
+        var version = versions?.First();
+        bool success = false;
+        if (version != null)
+        {
+            VersionFileDetails? file = version?.Files.FirstOrDefault(i => i.Primary);
+            if (file != null)
+            {
+                await client.DownloadVersionFile(file.Value, Path.Combine(Directory.GetParent(Process.GetCurrentProcess().MainModule?.FileName ?? "")?.FullName ?? Environment.CurrentDirectory), (s, e) =>
+                                {
+                                    Console.WriteLine(e.Percentage.ToString("P2"));
+                                });
+                success = true;
+            }
+        }
+        Console.ForegroundColor = !success ? ConsoleColor.Red : ConsoleColor.Green;
+        Console.Write($"[{(!success ? "FAIL" : "SUCCESS")}]");
         Console.ResetColor();
         Console.WriteLine($" Get Project Versions!");
     }
