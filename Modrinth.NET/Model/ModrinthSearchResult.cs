a1641ba9d07a2c76789216f83d67b89111a5c569
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 15:03:09 -0400

    added json property

--- Modrinth.NET/Model/ModrinthSearchResult.cs
+++ Modrinth.NET/Model/ModrinthSearchResult.cs
@@ -1,4 +1,6 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
 namespace Chase.Minecraft.Modrinth.Model;
 
 public struct ModrinthSearchResult
@@ -6,5 +8,7 @@ public struct ModrinthSearchResult
     public ModrinthSearchResultItem[] Hits { get; set; }
     public int Offset { get; set; }
     public int Limit { get; set; }
+
+    [JsonProperty("total_hits")]
     public int TotalHits { get; set; }
 }
