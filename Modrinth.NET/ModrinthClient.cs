d4ed2303c2be2df93b3cec3b3b171e578bfe4478
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 13:40:55 -0400

    added ModrinthClient for handling api requests

---
+++ Modrinth.NET/ModrinthClient.cs
@@ -0,0 +1,18 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Chase.Networking;
+
+namespace Chase.Minecraft.Modrinth;
+
+public abstract class ModrinthClient : NetworkClient
+{
+    private readonly string API_KEY = "";
+
+    public ModrinthClient(string api_key) : this()
+    {
+        API_KEY = api_key;
+    }
+
+    public ModrinthClient()
+    {
+    }
+}
