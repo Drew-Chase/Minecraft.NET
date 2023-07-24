301ee97af94ea68ad81b0f1b9186ec0e3f74a4e2
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 20:47:11 -0400

    added search results

---
+++ CurseForge.NET/Model/CurseforgeSearchResult.cs
@@ -0,0 +1,21 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Curseforge.Model;
+
+public struct CurseforgeSearchResult
+{
+    [JsonProperty("data")]
+    public CurseforgeProject[] Projects { get; set; }
+
+    [JsonProperty("pagination")]
+    public CurseforgeSearchPagination Pagination { get; set; }
+}
+
+public struct CurseforgeSearchPagination
+{
+    public int Index { get; set; }
+    public int PageSize { get; set; }
+    public int ResultCount { get; set; }
+    public int TotalCount { get; set; }
+}
