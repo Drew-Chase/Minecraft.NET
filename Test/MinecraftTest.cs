4446d413f2493c049a76106a6d557be8d9c3351a
Drew Chase <dcmanproductions@gmail.com>
2023-07-24 17:24:24 -0400

    added launch test

--- Test/MinecraftTest.cs
+++ Test/MinecraftTest.cs
@@ -10,26 +10,30 @@ internal static class MinecraftTest
     public static async Task Start()
     {
         var watch = Stopwatch.StartNew();
-        using MinecraftClient client = new(new() { Directory = "", JVMArguments = "", RAM = new() { } });
-        MinecraftVersion? version = await client.GetLatestMinecraftVersionAsync();
-        if (version != null)
+        ClientStartInfo info = new() { OfflineUsername = "dev", Directory = Directory.GetParent(Process.GetCurrentProcess().MainModule?.FileName ?? "")?.FullName ?? Environment.CurrentDirectory, JavaExecutable = Path.Combine(JavaController.GetLocalJVMInstallations()[0], "java.exe") };
+        //using MinecraftClient client = new(info);
+        //MinecraftVersion? version = await client.GetLatestMinecraftVersionAsync();
+        //if (version != null)
+        //{
+        //    client.SetMinecraftVersion(version.Value);
+        //    client.Start();
+        //}
+        MinecraftClient.Launch(info, "1.20.1", (s, e) =>
         {
-            Task.WaitAll(DownloadLibraries(client, version.Value), DownloadAssets(client, version.Value));
-        }
+            string? data = e.Data;
+            if (data != null)
+            {
+                Console.WriteLine(data);
+            }
+        }).WaitForExit();
         Console.WriteLine("Done");
         await Console.Out.WriteLineAsync($"Process took: {watch.Elapsed}");
         Console.ReadLine();
     }
 
-    private static async Task DownloadLibraries(MinecraftClient client, MinecraftVersion version)
+    private static async Task DownloadJava()
     {
-        await Console.Out.WriteLineAsync("Downloading Libraries");
-        await client.DownloadLibraries(version);
-    }
-
-    private static async Task DownloadAssets(MinecraftClient client, MinecraftVersion version)
-    {
-        await Console.Out.WriteLineAsync("Downloading Assets");
-        await client.DownloadAssets(version);
+        await Console.Out.WriteLineAsync("Downloading Java");
+        await JavaController.DownloadJava("java");
     }
 }
