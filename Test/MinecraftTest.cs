4c8dd893179cdf2d5d57e90bbbe4a7d05639b5ed
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 21:04:48 -0400

    seperated to individual functions

--- Test/MinecraftTest.cs
+++ Test/MinecraftTest.cs
@@ -7,10 +7,20 @@ internal static class MinecraftTest
 {
     public static async Task Start()
     {
-        MinecraftClient minecraftClient = new MinecraftClient(new(){Directory = "", JVMArguments = "", RAM = new() {} });
-        //await minecraftClient.DownloadAllLibs(new Uri("https://piston-meta.mojang.com/v1/packages/715ccf3330885e75b205124f09f8712542cbe7e0/1.20.1.json"));
-        await minecraftClient.DownloadAllAssets(new Uri("https://piston-meta.mojang.com/v1/packages/9d58fdd2538c6877fb5c5c558ebc60ee0b6d0e84/5.json"));
+        using MinecraftClient client = new(new() { Directory = "", JVMArguments = "", RAM = new() { } });
+        Task.WaitAll(DownloadLibraries(client), DownloadAssets(client));
         Console.WriteLine("Done");
         Console.ReadLine();
+    }
+
+    private static async Task DownloadLibraries(MinecraftClient client)
+    {
+        await Console.Out.WriteLineAsync("Downloading Libraries");
+        await client.DownloadAllLibs(new Uri("https://piston-meta.mojang.com/v1/packages/715ccf3330885e75b205124f09f8712542cbe7e0/1.20.1.json"));
+    }
+
+    private static async Task DownloadAssets(MinecraftClient client)
+    {
+        await client.DownloadAllAssets(new Uri("https://piston-meta.mojang.com/v1/packages/9d58fdd2538c6877fb5c5c558ebc60ee0b6d0e84/5.json"));
     }
 }
