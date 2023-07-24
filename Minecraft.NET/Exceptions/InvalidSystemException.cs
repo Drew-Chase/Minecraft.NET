a262921e3b6118737486205d21f1bb018171a6f6
Drew Chase <dcmanproductions@gmail.com>
2023-07-24 17:23:58 -0400

    added exception for if the system is invalid

---
+++ Minecraft.NET/Exceptions/InvalidSystemException.cs
@@ -0,0 +1,13 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace Chase.Minecraft.Exceptions;
+
+public class InvalidSystemException : Exception
+{
+    public InvalidSystemException(Dictionary<OSPlatform, ProcessorArchitecture> SupportedPlatforms) : base($"Unsupported System: {RuntimeInformation.OSDescription}-{RuntimeInformation.ProcessArchitecture}; Supported platforms: {JsonConvert.SerializeObject(SupportedPlatforms)}")
+    {
+    }
+}
