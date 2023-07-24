229fe46839dc737103c5581a41bea85c6e3a6724
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 20:47:02 -0400

    added searches

--- CurseForge.NET/CurseforgeClient.cs
+++ CurseForge.NET/CurseforgeClient.cs
@@ -1,11 +1,13 @@
 ﻿// LFInteractive LLC. 2021-2024﻿
+using Chase.Minecraft.Curseforge.Model;
 using Chase.Networking;
+using Newtonsoft.Json.Linq;
 
 namespace Chase.Minecraft.Curseforge;
 
 public class CurseforgeClient : IDisposable
 {
-    private static readonly string BASE_URI = "api.curseforge.com";
+    private static readonly string BASE_URI = "https://api.curseforge.com/v1/";
     private static readonly int GAME_ID = 432;
     private static readonly int FORGE_MODLOADER_ID = 1;
     private static readonly int FABRIC_MODLOADER_ID = 4;
@@ -28,5 +30,29 @@ public class CurseforgeClient : IDisposable
     public void Dispose()
     {
         _client.Dispose();
+    }
+
+    public Task<CurseforgeSearchResult?> SearchModsAsync(string query, string gameVersion, ModLoaders loaders) => SearchAsync(query, MODS_SECTION_ID, gameVersion, loaders);
+
+    public Task<CurseforgeSearchResult?> SearchModpackAsync(string query, string gameVersion, ModLoaders loaders) => SearchAsync(query, MODPACKS_SECTION_ID, gameVersion, loaders);
+
+    public Task<CurseforgeSearchResult?> SearchWorldsAsync(string query, string gameVersion, ModLoaders loaders) => SearchAsync(query, WORLDS_SECTION_ID, gameVersion, loaders);
+
+    public Task<CurseforgeSearchResult?> SearchResourcepacksAsync(string query, string gameVersion, ModLoaders loaders) => SearchAsync(query, RESOURCE_PACKS_SECTION_ID, gameVersion, loaders);
+
+    private async Task<CurseforgeSearchResult?> SearchAsync(string query, int classId, string gameVersion, ModLoaders loader)
+    {
+        using HttpRequestMessage request = new(HttpMethod.Get, $"{BASE_URI}mods/search?searchFilter={query}&gameId={GAME_ID}&classId={classId}&modLoaderType={loader}&gameVersion={gameVersion}");
+        request.Headers.Add("x-api-key", _api);
+        HttpResponseMessage response = await _client.SendAsync(request);
+        if (response.IsSuccessStatusCode)
+        {
+            CurseforgeSearchResult? result = JObject.Parse(await response.Content.ReadAsStringAsync())?.ToObject<CurseforgeSearchResult>();
+            if (result != null)
+            {
+                return result.Value;
+            }
+        }
+        return null;
     }
 }
