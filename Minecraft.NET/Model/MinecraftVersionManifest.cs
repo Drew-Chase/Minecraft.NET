dc28b7d3b8b2239b0ad4b5e1b67b7a28f5db1838
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 21:20:25 -0400

    added manifest structure

---
+++ Minecraft.NET/Model/MinecraftVersionManifest.cs
@@ -0,0 +1,18 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+namespace Chase.Minecraft.Model;
+
+public struct MinecraftVersionManifest
+{
+    public string Release { get; set; }
+    public string Snapshot { get; set; }
+    public MinecraftVersion[] Versions { get; set; }
+}
+
+public struct MinecraftVersion
+{
+    public string ID { get; set; }
+    public string Type { get; set; }
+    public Uri URL { get; set; }
+    public DateTime Time { get; set; }
+    public DateTime ReleaseTime { get; set; }
+}
