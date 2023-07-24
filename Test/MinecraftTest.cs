7870615791be274f07f5de35b7b442f64daa647d
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 21:20:43 -0400

    renamed functions

--- Test/MinecraftTest.cs
+++ Test/MinecraftTest.cs
@@ -16,11 +16,11 @@ internal static class MinecraftTest
     private static async Task DownloadLibraries(MinecraftClient client)
     {
         await Console.Out.WriteLineAsync("Downloading Libraries");
-        await client.DownloadAllLibs(new Uri("https://piston-meta.mojang.com/v1/packages/715ccf3330885e75b205124f09f8712542cbe7e0/1.20.1.json"));
+        await client.DownloadLibraries(new Uri("https://piston-meta.mojang.com/v1/packages/715ccf3330885e75b205124f09f8712542cbe7e0/1.20.1.json"));
     }
 
     private static async Task DownloadAssets(MinecraftClient client)
     {
-        await client.DownloadAllAssets(new Uri("https://piston-meta.mojang.com/v1/packages/9d58fdd2538c6877fb5c5c558ebc60ee0b6d0e84/5.json"));
+        await client.DownloadAssets(new Uri("https://piston-meta.mojang.com/v1/packages/9d58fdd2538c6877fb5c5c558ebc60ee0b6d0e84/5.json"));
     }
 }
