f4565d346fbced52041717dc9f3c48c38f7c3041
Drew Chase <dcmanproductions@gmail.com>
2023-07-23 20:46:52 -0400

    added selector

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,8 +6,21 @@ internal class Program
 {
     private static async Task Main()
     {
-        //await ModrinthTest.Start();
-        //await CurseforgeTest.Start();
-        await MinecraftTest.Start();
+        Console.Write("Test What?\n0 = Modrinth\n1 = CurseForge\n2 = Minecraft\nIndex: ");
+        string input = Console.ReadLine();
+        switch (input)
+        {
+            case "0":
+                await ModrinthTest.Start();
+                break;
+
+            case "1":
+                await CurseforgeTest.Start();
+                break;
+
+            case "2":
+                await MinecraftTest.Start();
+                break;
+        }
     }
 }
