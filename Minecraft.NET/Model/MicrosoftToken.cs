535a52266264468229d3529ac985fa0dd84b8032
Drew Chase <dcmanproductions@gmail.com>
2023-07-25 15:17:07 -0400

    added structures for Microsoft Authentication

---
+++ Minecraft.NET/Model/MicrosoftToken.cs
@@ -0,0 +1,28 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Model;
+
+public struct MicrosoftToken
+{
+    [JsonProperty("token_type")]
+    public string TokenType { get; set; }
+
+    [JsonProperty("expires_in")]
+    public long ExpiresIn { get; set; }
+
+    [JsonProperty("scope")]
+    public string Scope { get; set; }
+
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("refresh_token")]
+    public string RefreshToken { get; set; }
+
+    [JsonProperty("user_id")]
+    public string UserId { get; set; }
+
+    [JsonProperty("foci")]
+    public string Foci { get; set; }
+}
---
+++ Minecraft.NET/Model/XboxLiveAuthResponse.cs
@@ -0,0 +1,32 @@
+﻿// LFInteractive LLC. 2021-2024﻿
+using Newtonsoft.Json;
+using System.Text.Json;
+
+namespace Chase.Minecraft.Model;
+
+public struct XboxLiveAuthResponse
+{
+    [JsonProperty("NotAfter")]
+    public DateTime NotAfter;
+
+    [JsonProperty("IssueInstant")]
+    public DateTime IssueInstant { get; set; }
+
+    [JsonProperty("Token")]
+    public string Token { get; set; }
+
+    [JsonProperty("DisplayClaims")]
+    public XboxLiveDisplayClaims DisplayClaims { get; set; }
+}
+
+public struct XboxLiveDisplayClaims
+{
+    [JsonProperty("xui")]
+    public List<XboxLiveXUIClaim> XUI { get; set; }
+}
+
+public struct XboxLiveXUIClaim
+{
+    [JsonProperty("uhs")]
+    public string UHS { get; set; }
+}
