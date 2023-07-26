22ead9a6c95d15cef8e2528ab8db43314b928fcf
Drew Chase <dcmanproductions@gmail.com>
2023-07-26 01:00:35 -0400

    removed redundant information

--- Minecraft.NET/Model/ClientInfo.cs
+++ Minecraft.NET/Model/ClientInfo.cs
@@ -9,16 +9,12 @@ namespace Chase.Minecraft.Model;
 
 internal sealed class ClientInfo
 {
-    public ClientStartInfo ClientStartInfo { get; set; }
     public string Assets { get; set; }
     public string AssetIndex { get; set; }
     public string Libraries { get; set; }
-    public string ClientJar { get; set; }
     public string AuthenticationToken { get; set; } = "0";
-    public string InstanceDirectory { get; set; }
     public string ClientID { get; set; } = "0";
     public string ClientVersion { get; set; } = "0.0.0";
     public string ClientName { get; set; } = "Minecraft.NET";
-    public MinecraftVersion Version { get; set; }
     public DownloadArtifact[] LibraryFiles { get; set; }
 }
