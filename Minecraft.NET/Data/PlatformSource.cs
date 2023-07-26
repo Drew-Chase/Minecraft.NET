9d6097144a4747f975d0e13c8afb559ad84eaabd
Drew Chase <dcmanproductions@gmail.com>
2023-07-26 18:31:49 -0400

    added platform source

---
+++ Minecraft.NET/Data/PlatformSource.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
+    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+namespace Chase.Minecraft.Data;
+
+public enum PlatformSource
+{
+    Unknown,
+    Curseforge,
+    Modrinth,
+    ATLauncher,
+    Technic,
+}
