57a327831a5ed51900fef24ad2079f071070bdb2
Drew Chase <dcmanproductions@gmail.com>
2023-07-26 19:40:30 -0400

    added GetLoaderVersions function, Installer will now install downloaded installer

--- MinecraftFabric.NET/FabricLoader.cs
+++ MinecraftFabric.NET/FabricLoader.cs
@@ -5,31 +5,46 @@
     https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
 */
 
-using Chase.Minecraft.Instances;
+using Chase.Minecraft.Data;
 using Chase.Minecraft.Model;
 using Chase.Networking;
 using HtmlAgilityPack;
+using System.Diagnostics;
 
 namespace Chase.Minecraft.Fabric;
 
 public static class FabricLoader
 {
-    private static string maven = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/";
+    private static readonly string MavenURL = "https://maven.fabricmc.net";
+    private static readonly string MavenInstallerPath = "/net/fabricmc/fabric-installer/";
 
-    public static async Task Install(string version, InstanceManager manager, InstanceModel instance)
+    public static async Task Install(string version, InstanceModel instance)
     {
         using NetworkClient client = new();
 
         string? latestInstallerVersion = await GetLatestInstallerVersion(client);
         if (latestInstallerVersion != null)
         {
-            await client.DownloadFileAsync(new($"{maven}{latestInstallerVersion}"), "", (s, e) => { });
+            string temp = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
+            await client.DownloadFileAsync(new($"{MavenURL}{MavenInstallerPath}{latestInstallerVersion}"), temp, (s, e) => { });
             instance.ModLoader = new()
             {
                 Modloader = ModLoaders.Fabric,
                 Version = version
             };
-            manager.Save(instance.Id, instance);
+            instance.JVMArguments += " -DFabricMcEmu= net.minecraft.client.main.Main";
+            instance.JVMArguments = instance.JVMArguments.Trim();
+            instance.InstanceManager.Save(instance.Id, instance);
+            await Task.Run(() =>
+            {
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = instance.Java,
+                    Arguments = $"-jar {temp} client -dir \"{instance.Path}\" -loader {version} -noprofile",
+                    UseShellExecute = true,
+                    CreateNoWindow = true,
+                })?.WaitForExit();
+            });
         }
         else
         {
@@ -37,9 +52,37 @@ public static class FabricLoader
         }
     }
 
+    public static async Task<string[]> GetLoaderVersions()
+    {
+        using NetworkClient client = new();
+        using HttpResponseMessage response = await client.GetAsync(MavenURL + "/net/fabricmc/fabric-loader/");
+        if (response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            HtmlDocument document = new();
+            document.LoadHtml(content);
+            if (document.DocumentNode != null)
+            {
+                List<string> versions = new();
+                HtmlNodeCollection nodes = document.DocumentNode.SelectNodes("/html/body/pre/a");
+                foreach (HtmlNode node in nodes)
+                {
+                    string innerText = node.InnerText.Trim('/');
+                    if (int.TryParse(innerText[0].ToString(), out _))
+                    {
+                        versions.Add(innerText);
+                    }
+                }
+                versions.Sort(new VersionStringComparer());
+                return versions.ToArray();
+            }
+        }
+        return Array.Empty<string>();
+    }
+
     private static async Task<string?> GetLatestInstallerVersion(NetworkClient client)
     {
-        using HttpResponseMessage response = await client.GetAsync(maven);
+        using HttpResponseMessage response = await client.GetAsync(MavenURL);
         if (response.IsSuccessStatusCode)
         {
             string content = await response.Content.ReadAsStringAsync();
