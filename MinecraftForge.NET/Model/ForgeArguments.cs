e0f4a350d86f0880641e7727d949a8f18961dfa0
Drew Chase <dcmanproductions@gmail.com>
2023-07-27 15:00:13 -0400

    added forge version translation

---
+++ MinecraftForge.NET/Model/ForgeArguments.cs
@@ -0,0 +1,19 @@
+﻿/*
+    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
+    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Forge.Model;
+
+public struct ForgeArguments
+{
+    [JsonProperty("game")]
+    public string[] Game { get; set; }
+
+    [JsonProperty("jvm")]
+    public string[] Jvm { get; set; }
+}
---
+++ MinecraftForge.NET/Model/ForgeVersionInfo.cs
@@ -0,0 +1,37 @@
+﻿/*
+    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
+    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+using Newtonsoft.Json;
+
+namespace Chase.Minecraft.Forge.Model;
+
+public struct ForgeVersionInfo
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("time")]
+    public DateTime Time { get; set; }
+
+    [JsonProperty("releaseTime")]
+    public DateTime ReleaseTime { get; set; }
+
+    [JsonProperty("type")]
+    public string Type { get; set; }
+
+    [JsonProperty("mainClass")]
+    public string MainClass { get; set; }
+
+    [JsonProperty("inheritsFrom")]
+    public string InheritsFrom { get; set; }
+
+    [JsonProperty("arguments")]
+    public ForgeArguments Arguments { get; set; }
+
+    [JsonProperty("libraries")]
+    public ForgeLibrary[] Libraries { get; set; }
+}
