727971a4c4dbfe7148ad672a5c4c04ace4fd3a4a
Drew Chase <dcmanproductions@gmail.com>
2023-07-27 21:07:10 -0400

    added getters and downloaders

--- CurseForge.NET/Controller/CurseforgeClient.cs
+++ CurseForge.NET/Controller/CurseforgeClient.cs
@@ -6,7 +6,9 @@
 */
 
 using Chase.Minecraft.Curseforge.Model;
+using Chase.Minecraft.Model;
 using Chase.Networking;
+using Chase.Networking.Event;
 
 namespace Chase.Minecraft.Curseforge.Controller;
 
@@ -39,6 +41,45 @@ public class CurseforgeClient : IDisposable
     public Task<CurseforgeSearchResult?> SearchWorldsAsync(string query, string gameVersion) => SearchAsync(query, WORLDS_SECTION_ID, gameVersion, null);
 
     public Task<CurseforgeSearchResult?> SearchResourcepacksAsync(string query, string gameVersion) => SearchAsync(query, RESOURCE_PACKS_SECTION_ID, gameVersion, null);
+
+    public Task<CurseforgeProject?> GetMod(string id) => GetProject(id, MODS_SECTION_ID);
+
+    public Task<CurseforgeProject?> GetModpack(string id) => GetProject(id, MODPACKS_SECTION_ID);
+
+    public Task<CurseforgeProject?> GetResourcepack(string id) => GetProject(id, RESOURCE_PACKS_SECTION_ID);
+
+    public Task<CurseforgeProject?> GetWorld(string id) => GetProject(id, WORLDS_SECTION_ID);
+
+    public Task<string> Download(ModFile mod, InstanceModel instance, string subfolder = "mods", DownloadProgressEvent? progressEvent = null) => Download(mod, Path.Combine(instance.Path, subfolder), progressEvent);
+
+    public async Task<string> Download(ModFile mod, string directory, DownloadProgressEvent? progressEvent = null)
+    {
+        string path = Path.Combine(directory, mod.FileName);
+        await _client.DownloadFileAsync(mod.DownloadUrl, path, progressEvent);
+        return path;
+    }
+
+    public Task<ModFile[]?> GetModFiles(string id) => GetProjectFiles(id, MODS_SECTION_ID);
+
+    public Task<ModFile[]?> GetModpackFiles(string id) => GetProjectFiles(id, MODPACKS_SECTION_ID);
+
+    public Task<ModFile[]?> GetResourcepackFiles(string id) => GetProjectFiles(id, RESOURCE_PACKS_SECTION_ID);
+
+    public Task<ModFile[]?> GetWorldFiles(string id) => GetProjectFiles(id, WORLDS_SECTION_ID);
+
+    public Task<ModFile?> GetModFile(string id, string fileId) => GetProjectFile(id, fileId, MODS_SECTION_ID);
+
+    public Task<ModFile?> GetModpackFile(string id, string fileId) => GetProjectFile(id, fileId, MODPACKS_SECTION_ID);
+
+    public Task<ModFile?> GetResourcepackFile(string id, string fileId) => GetProjectFile(id, fileId, RESOURCE_PACKS_SECTION_ID);
+
+    public Task<ModFile?> GetWorldFile(string id, string fileId) => GetProjectFile(id, fileId, WORLDS_SECTION_ID);
+
+    private async Task<CurseforgeProject?> GetProject(string id, int classId) => (await _client.GetAsJson($"{BASE_URI}mods/{id}?gameId={GAME_ID}&classId={classId}"))?["data"]?.ToObject<CurseforgeProject>();
+
+    private async Task<ModFile[]?> GetProjectFiles(string id, int classId) => (await _client.GetAsJson($"{BASE_URI}mods/{id}/files?gameId={GAME_ID}&classId={classId}"))?["data"]?.ToObject<ModFile[]>();
+
+    private async Task<ModFile?> GetProjectFile(string id, string fileId, int classId) => (await _client.GetAsJson($"{BASE_URI}mods/{id}/files/{fileId}?gameId={GAME_ID}&classId={classId}"))?["data"]?.ToObject<ModFile>();
 
     private async Task<CurseforgeSearchResult?> SearchAsync(string query, int classId, string gameVersion, ModLoaders? loader)
     {
