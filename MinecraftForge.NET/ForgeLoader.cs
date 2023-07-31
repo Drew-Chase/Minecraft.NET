e73c9f0a03d5889abf6589abbbd495cc678a95a7
Drew Chase <dcmanproductions@gmail.com>
2023-07-31 16:46:06 -0400

    added ability to parse ModToml, added call to ForgeWrapper.jar to install forge, added the forge libraries to the additional paths path

--- MinecraftForge.NET/ForgeLoader.cs
+++ MinecraftForge.NET/ForgeLoader.cs
@@ -12,19 +12,100 @@ using Chase.Networking;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using Serilog;
+using System.Diagnostics;
 using System.IO.Compression;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Chase.Minecraft.Forge;
 
 public static class ForgeLoader
 {
-    public static async Task<InstanceModel?> Install(string loader_version, InstanceModel instance)
+    public static ModToml? GetLoaderFile(string jar)
+    {
+        using ZipArchive archive = ZipFile.OpenRead(jar);
+        foreach (ZipArchiveEntry entry in archive.Entries)
+        {
+            if (entry.FullName.Equals("META-INF/mods.toml", StringComparison.OrdinalIgnoreCase))
+            {
+                using Stream stream = entry.Open();
+                using StreamReader reader = new(stream);
+                string data = reader.ReadToEnd();
+
+                // Define regex patterns
+                var patternKeyValue = new Regex(@"(\S+)\s*=\s*[""']?(.*?)[""']?$");
+                var patternSection = new Regex(@"^\[([^]]+)\]$");
+
+                // Parse the data using regex
+                var sections = new Dictionary<string, object>();
+                string currentSection = null;
+                List<Dictionary<string, string>> currentList = null;
+                Dictionary<string, string> currentObject = null;
+
+                foreach (string line in data.Split('\n'))
+                {
+                    var trimmedLine = line.Trim();
+                    if (patternSection.IsMatch(trimmedLine))
+                    {
+                        currentSection = patternSection.Match(trimmedLine).Groups[1].Value;
+                        if (currentSection.EndsWith("s"))
+                        {
+                            currentList = new List<Dictionary<string, string>>();
+                            sections[currentSection] = currentList;
+                        }
+                        else
+                        {
+                            currentObject = new Dictionary<string, string>();
+                            sections[currentSection] = currentObject;
+                        }
+                    }
+                    else if (patternKeyValue.IsMatch(trimmedLine))
+                    {
+                        var match = patternKeyValue.Match(trimmedLine);
+                        string key = match.Groups[1].Value;
+                        string value = match.Groups[2].Value;
+
+                        if (currentSection == null)
+                        {
+                            sections[key] = value;
+                        }
+                        else if (sections[currentSection] is List<Dictionary<string, string>> list)
+                        {
+                            if (currentObject != null)
+                            {
+                                list.Add(currentObject);
+                                currentObject = null;
+                            }
+                            var lastObject = list.LastOrDefault();
+                            if (lastObject == null || lastObject.ContainsKey(key))
+                            {
+                                currentObject = new Dictionary<string, string>();
+                                list.Add(currentObject);
+                            }
+                            list.LastOrDefault()?.Add(key, value);
+                        }
+                        else if (sections[currentSection] is Dictionary<string, string> dict)
+                        {
+                            dict[key] = value;
+                        }
+                    }
+                }
+
+                // Convert to JSON
+                string json = JsonConvert.SerializeObject(sections, Formatting.Indented);
+                Console.WriteLine(json);
+            }
+        }
+        return null;
+    }
+
+    public static async Task<InstanceModel?> Install(string loaderVersion, InstanceModel instance, string rootDirectory, string javaExecutable)
     {
         using NetworkClient client = new();
         string installerPath = Path.Combine(Path.GetTempPath(), Path.GetTempFileName());
-        Uri url = new($"https://maven.minecraftforge.net/net/minecraftforge/forge/{instance.MinecraftVersion.ID}-{loader_version}/forge-{instance.MinecraftVersion.ID}-{loader_version}-installer.jar");
+        Uri url = new($"https://maven.minecraftforge.net/net/minecraftforge/forge/{instance.MinecraftVersion.ID}-{loaderVersion}/forge-{instance.MinecraftVersion.ID}-{loaderVersion}-installer.jar");
         await client.DownloadFileAsync(url, installerPath, (_, _) => { });
-
+        string wrapper = ExtractWrapper();
         string jsonFileName = "version.json";
         ForgeVersionInfo info = new();
         using (ZipArchive archive = ZipFile.OpenRead(installerPath))
@@ -43,26 +124,89 @@ public static class ForgeLoader
         if (!string.IsNullOrWhiteSpace(info.Id))
         {
             instance.MinecraftArguments = info.Arguments.Game;
-            instance.JVMArguments = info.Arguments.Jvm.Select(i => i.Replace("${library_directory}", Path.Combine(instance.Path, "libraries")).Replace("${classpath_separator}", ";").Replace("${version_name}", info.Id)).ToArray();
+            string librariesPath = Path.Combine(rootDirectory, "libraries");
+            instance.JVMArguments = info.Arguments.Jvm.Select(i => i.Replace("${library_directory}", librariesPath).Replace("${classpath_separator}", ";").Replace("${version_name}", info.Id)).ToArray();
             instance.ModLoader = new()
             {
                 Modloader = ModLoaders.Forge,
-                Version = loader_version
+                Version = loaderVersion
             };
 
+            string fmlVersion = "";
+
+            for (int i = 0; i < instance.JVMArguments.Length; i++)
+            {
+                string name = instance.JVMArguments[i];
+                if (name == "-p")
+                {
+                    instance.JVMArguments[i + 1] = $"\"{instance.JVMArguments[i + 1]}\"";
+                }
+                if (name.StartsWith("-DlibraryDirectory="))
+                {
+                    instance.JVMArguments[i] = $"-DlibraryDirectory=\"{librariesPath}\"";
+                }
+            }
+
+            for (int i = 0; i < info.Arguments.Game.Length; i++)
+            {
+                string name = info.Arguments.Game[i];
+
+                if (name == "--fml.mcpVersion")
+                {
+                    fmlVersion = info.Arguments.Game[i + 1];
+                }
+            }
+
             List<Task> tasks = new();
+            List<string> paths = new();
             foreach (ForgeLibrary libraryItem in info.Libraries)
             {
                 ForgeArtifact artifact = libraryItem.Downloads.Artifact;
                 Uri artifactUri = artifact.Url;
-                string artifactPath = Path.Combine(Path.Combine(instance.Path, "libraries"), artifact.Path);
+                string artifactPath = Path.Combine(librariesPath, artifact.Path);
                 Directory.CreateDirectory(Directory.GetParent(artifactPath)?.FullName ?? "");
-                Log.Debug($"[Forge] Downloading {artifact.Path}");
-                tasks.Add(client.DownloadFileAsync(artifactUri, artifactPath, (_, _) => { }));
+                paths.Add(artifactPath);
             }
             Task.WaitAll(tasks.ToArray());
+            instance.LaunchClassPath = info.MainClass;
+            instance.AdditionalClassPaths = paths.ToArray();
+            instance.ClientJar = Path.Combine(rootDirectory, "versions", info.Id, $"{info.Id}.jar");
+            instance.GameVersion = info.Id;
+            instance = instance.InstanceManager.Save(instance.Id, instance);
 
-            instance.InstanceManager.Save(instance.Id, instance);
+            Process process = new()
+            {
+                StartInfo = new()
+                {
+                    FileName = javaExecutable,
+                    Arguments = $"-jar \"{wrapper}\" -i \"{installerPath}\" -o \"{rootDirectory}\"",
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                },
+                EnableRaisingEvents = true,
+            };
+            process.OutputDataReceived += (s, e) =>
+            {
+                string? data = e.Data;
+                if (data != null)
+                {
+                    Log.Debug("[FORGE] " + data);
+                }
+            };
+            process.ErrorDataReceived += (s, e) =>
+            {
+                string? data = e.Data;
+                if (data != null)
+                {
+                    Log.Error("[FORGE] " + data);
+                }
+            };
+
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
 
             return instance;
         }
@@ -96,5 +240,22 @@ public static class ForgeLoader
             }
         }
         return Array.Empty<string>();
+    }
+
+    private static string ExtractWrapper()
+    {
+        string path = Path.Combine(Path.GetTempPath(), "forge-wrapper.jar");
+
+        if (!File.Exists(path))
+        {
+            using Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("Chase.Minecraft.Forge.jars.ForgeWrapper.jar");
+            if (stream != null)
+            {
+                using FileStream fs = new(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+                stream.CopyTo(fs);
+            }
+        }
+
+        return path;
     }
 }
---
+++ MinecraftForge.NET/Model/ModToml.cs
@@ -0,0 +1,40 @@
+﻿/*
+    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
+    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
+    Licensed under GPL-3.0
+    https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+*/
+
+namespace Chase.Minecraft.Forge.Model;
+
+public struct ModToml
+{
+    public string modLoader { get; set; }
+    public string loaderVersion { get; set; }
+    public string license { get; set; }
+    public string issueTrackerURL { get; set; }
+    public string displayURL { get; set; }
+    public string logoFile { get; set; }
+    public string authors { get; set; }
+    public List<ModEntry> mods { get; set; }
+}
+
+public sealed class ModEntry
+{
+    public string modId { get; set; }
+    public string version { get; set; }
+    public string displayName { get; set; }
+    public string updateJSONURL { get; set; }
+    public string displayTest { get; set; }
+    public string description { get; set; }
+    public List<Dependency> dependencies { get; set; }
+}
+
+public sealed class Dependency
+{
+    public string modId { get; set; }
+    public bool mandatory { get; set; }
+    public string versionRange { get; set; }
+    public string ordering { get; set; }
+    public string side { get; set; }
+}
