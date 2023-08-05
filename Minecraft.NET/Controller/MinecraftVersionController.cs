c1d0a513b4ddab23a33cadee939d0cee7e2bc971
Drew Chase <dcmanproductions@gmail.com>
2023-08-05 11:28:40 -0400

    updated modules

--- Minecraft.NET/Controller/MinecraftVersionController.cs
+++ Minecraft.NET/Controller/MinecraftVersionController.cs
@@ -1,8 +1,9 @@
 ﻿/*
-    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
-    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
-    Licensed under GPL-3.0
+    PolygonMC - LFInteractive LLC. 2021-2024
+    PolygonMC is a free and open source Minecraft Launcher implementing various modloaders, mod platforms, and minecraft authentication.
+    PolygonMC is protected under GNU GENERAL PUBLIC LICENSE version 3.0 License
     https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+    https://github.com/DcmanProductions/PolygonMC
 */
 
 using Chase.Minecraft.Model;
@@ -43,7 +44,8 @@ public static class MinecraftVersionController
         using HttpResponseMessage response = await client.GetAsync("https://launchermeta.mojang.com/mc/game/version_manifest.json");
         if (response.IsSuccessStatusCode)
         {
-            return JObject.Parse(await response.Content.ReadAsStringAsync())?.ToObject<MinecraftVersionManifest>();
+            string content = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(content)?.ToObject<MinecraftVersionManifest>();
         }
 
         return null;
