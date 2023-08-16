20c578bea4da5452678318a36ad0de0c16c679c5
Drew Chase <dcmanproductions@gmail.com>
2023-08-15 20:16:52 -0400

    updated min ram

--- Minecraft.NET/Model/RAMInfo.cs
+++ Minecraft.NET/Model/RAMInfo.cs
@@ -1,8 +1,9 @@
 ﻿/*
-    Minecraft.NET - LFInteractive LLC. 2021-2024﻿
-    Minecraft.NET and its libraries are a collection of minecraft related libraries to handle downloading mods, modpacks, resourcepacks, and downloading and installing modloaders (fabric, forge, etc)
-    Licensed under GPL-3.0
+    PolygonMC - LFInteractive LLC. 2021-2024
+    PolygonMC is a free and open source Minecraft Launcher implementing various modloaders, mod platforms, and minecraft authentication.
+    PolygonMC is protected under GNU GENERAL PUBLIC LICENSE version 3.0 License
     https://www.gnu.org/licenses/gpl-3.0.en.html#license-text
+    https://github.com/DcmanProductions/PolygonMC
 */
 
 using Newtonsoft.Json;
@@ -24,5 +25,5 @@ public sealed class RAMInfo
     /// Minimum ram allocation in megabytes
     /// </summary>
     [JsonProperty("min-ram")]
-    public int Minimum { get; set; } = 4096;
+    public int Minimum { get; set; } = 256;
 }
