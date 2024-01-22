50a77230b61643e7d7af6f4876267164d829608a
Drew Chase <dcmanproductions@gmail.com>
2024-01-21 19:13:44 -0500

    Add Modrinth import functionality

--- Modpack.NET/ModpackImporter.cs
+++ Modpack.NET/ModpackImporter.cs
@@ -10,9 +10,14 @@ using Chase.Minecraft.Fabric;
 using Chase.Minecraft.Forge;
 using Chase.Minecraft.Instances;
 using Chase.Minecraft.Model;
+using Chase.Minecraft.Modrinth.Controller;
+using Chase.Minecraft.Modrinth.Model;
+using Chase.Networking;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Serilog;
 using System.IO.Compression;
+using System.Text.RegularExpressions;
 
 namespace Chase.Minecraft.Modpacks;
 
@@ -94,4 +99,131 @@ public static class ModpackImporter
 
         return instance;
     }
+
+    public static async Task<InstanceModel> ImportModrinth(string file, InstanceManager manager, string javaPath, string rootDirectory, string curseforge_api)
+    {
+        ModrinthModpackModel modpackModel = new();
+        string overloadsDirectory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "LFInteractive", "Minecraft.NET", Path.GetRandomFileName())).FullName;
+        using (ZipArchive archive = ZipFile.OpenRead(file))
+        {
+            foreach (ZipArchiveEntry entry in archive.Entries)
+            {
+                if (entry.Name.Equals("modrinth.index.json", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    StreamReader reader = new(entry.Open());
+                    modpackModel = JObject.Parse(reader.ReadToEnd()).ToObject<ModrinthModpackModel>();
+                    break;
+                }
+            }
+            archive.ExtractToDirectory(overloadsDirectory);
+            overloadsDirectory = Path.Combine(overloadsDirectory, "overrides");
+        }
+
+        MinecraftVersion? minecraftVersion = MinecraftVersionController.GetMinecraftVersionByName(modpackModel.Dependencies.Minecraft);
+        if (minecraftVersion != null)
+        {
+            InstanceModel instance = manager.Create(new()
+            {
+                Name = modpackModel.Name,
+                Description = modpackModel.Summary,
+                GameVersion = modpackModel.Dependencies.Minecraft,
+                ModLoader = new()
+                {
+                    Modloader = !string.IsNullOrWhiteSpace(modpackModel.Dependencies.ForgeLoader) ? ModLoaders.Forge : !string.IsNullOrWhiteSpace(modpackModel.Dependencies.FabricLoader) ? ModLoaders.Fabric : ModLoaders.None,
+                    Version = !string.IsNullOrWhiteSpace(modpackModel.Dependencies.ForgeLoader) ? modpackModel.Dependencies.ForgeLoader : !string.IsNullOrWhiteSpace(modpackModel.Dependencies.FabricLoader) ? modpackModel.Dependencies.FabricLoader : "",
+                },
+                MinecraftVersion = minecraftVersion.Value,
+                Java = JavaController.GetValidJavaInstallationForMinecraftVersion(minecraftVersion.Value, javaPath),
+                Source = Data.PlatformSource.Modrinth,
+                JVMArguments = Array.Empty<string>(),
+                MinecraftArguments = Array.Empty<string>(),
+                AdditionalClassPaths = Array.Empty<string>(),
+                ClassPaths = Array.Empty<string>(),
+                RAM = new(),
+                Mods = Array.Empty<ModModel>(),
+            });
+
+
+            foreach (string entry in Directory.GetFileSystemEntries(overloadsDirectory, "*", SearchOption.TopDirectoryOnly))
+            {
+                FileInfo info = new(entry);
+                if (info.Attributes.HasFlag(FileAttributes.Directory))
+                {
+                    Directory.Move(entry, Path.Combine(instance.Path, info.Name));
+                }
+                else
+                {
+                    File.Move(entry, Path.Combine(instance.Path, info.Name));
+                }
+            }
+
+            List<ModModel> mods = new();
+            using (NetworkClient networkClient = new())
+            {
+                foreach (ModpackFile item in modpackModel.Files)
+                {
+                    if (item.Path.StartsWith("mods/"))
+                    {
+                        string url = item.Downloads.First();
+                        string projectId = "";
+                        string versionId = "";
+                        string filename = "";
+
+                        Match match = Regex.Match(url, @"/data/([^/]+)/versions/([^/]+)/([^/]+\.jar)");
+
+                        if (match.Success)
+                        {
+                            projectId = match.Groups[1].Value;
+                            versionId = match.Groups[2].Value;
+                            filename = match.Groups[3].Value;
+                        }
+
+                        mods.Add(new()
+                        {
+                            Name = filename,
+                            FileName = filename,
+                            ProjectID = projectId,
+                            VersionID = versionId,
+                            DownloadURL = url,
+                            Source = Data.PlatformSource.Modrinth,
+                        });
+                    }
+                    else
+                    {
+                        string path = Path.Combine(instance.Path, item.Path);
+                        string directory = Directory.CreateDirectory(Directory.GetParent(path).FullName).FullName;
+                        await networkClient.DownloadFileAsync(item.Downloads.First(), path);
+                    }
+                }
+            }
+
+            instance.Mods = mods.ToArray();
+
+            InstanceManager.ReDownloadMods(instance);
+
+            using (MinecraftClient client = new("", rootDirectory, instance))
+            {
+                Task.WaitAll(client.DownloadClient(), client.DownloadLibraries(), client.DownloadAssets());
+            }
+
+            if (instance.ModLoader.Modloader == ModLoaders.Fabric)
+            {
+                await FabricLoader.Install(instance.ModLoader.Version, instance);
+            }
+            else if (instance.ModLoader.Modloader == ModLoaders.Forge)
+            {
+                await ForgeLoader.Install(instance.ModLoader.Version, instance, rootDirectory, JavaController.GetLocalJVMInstallations(javaPath).Latest);
+            }
+
+            using (ModrinthClient client = new())
+            {
+                Log.Information("Mapped {COUNT} unmapped mods", ModpackUtils.MapUnMappedMods(instance, client, new Curseforge.Controller.CurseforgeClient(curseforge_api)));
+            }
+
+            return manager.Save(instance.Id, instance);
+        }
+        throw new Exception($"Unable to parse minecraft version, {modpackModel.Dependencies.Minecraft}");
+
+    }
+
 }
